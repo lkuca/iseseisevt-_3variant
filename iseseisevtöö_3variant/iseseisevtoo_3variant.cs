a491278fbac51da680fe9aebe84ddbf2cd33bf18
opilane <opilane@HP-CZC2349HTQ>
2023-04-24 12:17:35 +0300

    Add project files.

---
+++ iseseisevtöö_3variant/iseseisevtoo_3variant.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iseseisevtöö_3variant
+{
+    class iseseisevtoo_3variant
+    {
+        public static void Main(string[] args)
+            
+        {
+            //var array = new decimal[4];
+            //for (int i = 1; i < 4; i++)
+            //{
+            //    Console.Write("sissestage {0} arv : ", i + 1);
+            //    array[i] = Convert.ToDecimal(Console.ReadLine());
+            //}
+
+            //var max = array[0];
+            //var min = array[0];
+            //for (int i = 1; i < 4; i++)
+            //{
+            //    if (array[i] < min) min = array[i];
+            //    if (array[i] > max) max = array[i];
+            //}
+
+            //Console.WriteLine("erinevus vahel maksimaalsest ja minimaalsest = {0}", max - min);
+
+
+
+
+
+
+
+
+            //int[] array1 = new int[125];
+            //Random rand = new Random();
+            //int k = 0;
+            //for (int i = 0; i < array1.Length; i++)
+            //{
+            //    array1[i] = rand.Next(250);
+            //    Console.WriteLine(array1[i]);
+            //    int h = array1[i];
+            //    if ( i < 125)
+            //    {
+            //        foreach(var chislo in array1)
+            //        k++;
+            //    }
+                
+            //}
+            //Console.WriteLine("kokku {0}", k);
+            
+
+
+
+
+        }
+
+    }
+}
